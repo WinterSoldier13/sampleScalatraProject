fd6efb03d8133938fe370e3c29db880ed3b66535
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-11-10 00:10:25 +0530

    Added 1. GET 2. Add 3. UPDATE

--- sampleapp/src/main/scala/com/sample/app/MyScalatraServlet.scala
+++ sampleapp/src/main/scala/com/sample/app/MyScalatraServlet.scala
@@ -18,7 +18,7 @@ import org.scalatra.json._
 
 case class usersTable(id: Int, fname: String, lname: String, age: Int, dob: String)
 
-class MyScalatraServlet extends ScalatraServlet with JacksonJsonSupport{
+class MyScalatraServlet extends ScalatraServlet with JacksonJsonSupport {
     implicit lazy val jsonFormats: Formats = DefaultFormats
     before() {
         contentType = formats("json")
@@ -27,8 +27,8 @@ class MyScalatraServlet extends ScalatraServlet with JacksonJsonSupport{
         Class.forName("com.mysql.jdbc.Driver")
         val myConn = DriverManager.getConnection("JDBC:mysql://localhost:3306/sample_db", "ayush", "password")
         val myStmt = myConn.createStatement
-    
-    
+        
+        
         get("/api/getUsers/") {
             val users = myStmt.executeQuery("Select * from users")
             var usersArr: ListBuffer[usersTable] = new ListBuffer[_root_.com.sample.app.usersTable]
@@ -37,27 +37,103 @@ class MyScalatraServlet extends ScalatraServlet with JacksonJsonSupport{
                 users.next
             }) {
                 var temp: usersTable = usersTable(id = users.getString("id").toInt, fname = users.getString("fname"), lname = users.getString("lname"), age = users.getString("age").toInt, dob = users.getString("dob").toString)
-                usersArr+=temp
+                usersArr += temp
             }
-        
+            
             for (x <- usersArr) {
                 println(x.id, x.fname, x.lname, x.age, x.dob);
             }
             usersArr
         }
-        put("/api/create/:id") {
         
+        
+        put("/api/create/") {
+            //            GET THE JSON HERE
+            var a = parsedBody.extract[usersTable]
+            
+            val fname = a.fname.toString
+            val lname = a.lname.toString
+            val age: Int = a.age.toInt
+            val dob = a.dob.toString
+            
+            println(s"RECEIVED $fname $lname $age $dob")
+            var command: String = s"INSERT INTO users(fname, lname, age, dob) VALUES ( '$fname' , '$lname' , $age , '$dob')";
+            myStmt.executeUpdate(command)
+            
+            //            println(command)
+            println("Added to Table")
         }
+        
+        
         put("/api/update/:id") {
-        
+            val id_ = params {
+                "id"
+            }.toInt
+            println(s"ID RECEIVED : $id_")
+            
+            //            First I'll get the previous data stored in the DB
+            val command: String = s"SELECT * FROM users WHERE id=$id_"
+            val users = myStmt.executeQuery(command)
+            var initFname :String = "";
+            var initLname : String = "";
+            var initAge : Int = -1;
+            var initDOB : String = "";
+            
+            while ( {
+                users.next()
+            }) {
+                val temp: usersTable = usersTable(id = id_, fname = users.getString("fname"), lname = users.getString("lname"), age = users.getString("age").toInt, dob = users.getString("dob").toString)
+                 initFname = temp.fname.toString;
+                 initLname = temp.lname.toString;
+                 initAge = temp.age.toInt;
+                 initDOB = temp.dob.toString;
+            }
+            println(s"Previously Stored $initFname $initLname $initAge $initDOB")
+            
+            
+//            TIME TO UPDATE
+            var receivedJSON = parsedBody.extract[usersTable]
+            
+            var newFname = initFname;
+            var newLname = initLname;
+            var newAge = initAge;
+            var newDOB = initDOB;
+    
+            if(receivedJSON.fname != "")
+                {
+                    newFname = receivedJSON.fname.toString;
+                }
+            if(receivedJSON.lname != "")
+                {
+                    newLname = receivedJSON.lname.toString;
+                }
+            if(receivedJSON.age != None)
+                {
+                    newAge = receivedJSON.age.toInt
+                }
+            if(receivedJSON.dob != "")
+                {
+                    newDOB = receivedJSON.dob;
+                }
+            
+            var c :String = s"UPDATE users SET fname='$newFname', lname='$newLname', age=$newAge, dob='$newDOB' WHERE id=$id_"
+            myStmt.executeUpdate(c)
+            println("UPDATED")
+            
         }
-        put("/api/delete/:id") {
         
-        }
-    }
-    finally
+        
+        delete("/api/delete/:id")
         {
-            println("ERROR")
+            val id_ : Int = params{"id"}.toInt
+            val command = s"DELETE * FROM users WHERE id=$id_"
+            
+            myStmt.executeUpdate(command)
+            println(command)
         }
+    } // try ends here
+    finally {
+        println("ERROR")
+    }
     
 }
