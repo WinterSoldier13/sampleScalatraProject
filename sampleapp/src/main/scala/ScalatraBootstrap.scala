5e5df520735bc1c8e8f59e8350b7c2be14117e0b
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-11-09 16:33:30 +0530

    update

---
+++ sampleapp/src/main/scala/ScalatraBootstrap.scala
@@ -0,0 +1,13 @@
+import com.sample.app._
+import org.scalatra._
+import javax.servlet.ServletContext
+
+class ScalatraBootstrap extends LifeCycle
+{
+  override def init(context: ServletContext)
+  {
+    context.mount(new MyScalatraServlet, "/*")
+  }
+  
+  
+}
